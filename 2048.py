ef1cc25279b545c0a0473cfb10ee3ae60a58e0f3
Seid-2 <seid.fejzulahi@eduvaud.ch>
2025-03-17 14:10:32 +0100

    pour chez moi

--- 2048.py
+++ 2048.py
@@ -1,9 +1,100 @@
-from cgitb import reset
-from itertools import count
 from tkinter import *
 from tkinter import messagebox
 import random
 # 2 dimensions list with data
+
+'''
+
+numbers = [
+ [2, 4, 8, 16],
+ [32, 64, 128, 256],
+ [512, 1024, 2048, 0],
+ [0, 0, 0, 0],
+]
+'''
+
+numbers = [
+ [0, 0, 0, 0],
+ [0, 0, 0, 0],
+ [0, 0, 0, 0],
+ [0, 0, 0, 0],
+]
+
+numbers_backup = [ # copie du tableau avant chaque move
+ [0, 0, 0, 0],
+ [0, 0, 0, 0],
+ [0, 0, 0, 0],
+ [0, 0, 0, 0],
+]
+
+won = False
+
+def display_game():#Chat gpt je lui est demandé trouve moi le code
+    for line in range(len(numbers)):
+        for col in range(len(numbers[line])):
+            # Met à jour le texte et la couleur de fond du label en fonction de la valeur dans `numbers`
+            labels[line][col].config(
+                text=numbers[line][col] or "",
+                bg=colours.get(numbers[line][col], "#74E2AE")
+            )
+            # Affectation des touches aux fonctions, q pour quitter, le reste pour "tasser" dans une certaine direction
+
+def copy_numbers(my_list):
+    new_list = []
+    for line in my_list:
+        new_line = []
+        for col in line:
+            new_line.append(col)
+        new_list.append(new_line)
+    return new_list
+
+
+def key_pressed(event):
+    global won, numbers_backup, numbers
+    key = event.keysym  # récupérer le symbole de la touche
+
+    # code qui permet de quitter le jeu
+    if key == "Z" or key == "z":
+        result = messagebox.askokcancel("Confirmation", "Voulez-vous vraiment returner en arrière ?")
+        if result:
+            numbers = copy_numbers(numbers_backup)
+            display_game()
+
+    numbers_backup = copy_numbers(numbers)
+    moves = 0
+    #code qui permet de faire les mouvement gauche, droite, haut, bas avec les touches w,a,s,d
+    if key == "Right" or key == "d" or key == "D":
+        for line in range(len(numbers)):
+            numbers[line][3], numbers[line][2], numbers[line][1], numbers[line][0], move = pack4(numbers[line][3],numbers[line][2],numbers[line][1],numbers[line][0])
+            moves = moves + move
+    if key == "Left" or key == "a" or key == "A":
+        for line in range(len(numbers)):
+            numbers[line][0], numbers[line][1], numbers[line][2], numbers[line][3], move = pack4(numbers[line][0],numbers[line][1],numbers[line][2],numbers[line][3])
+            moves = moves + move
+    if key == "Up" or key == "w" or key == "W":
+        for col in range(len(numbers[0])):
+            numbers[0][col], numbers[1][col], numbers[2][col], numbers[3][col], move = pack4(numbers[0][col], numbers[1][col], numbers[2][col], numbers[3][col])
+            moves = moves + move
+    if key == "Down" or key == "s" or key == "S":
+        for col in range(len(numbers[0])):
+            numbers[3][col], numbers[2][col], numbers[1][col], numbers[0][col], move = pack4(numbers[3][col], numbers[2][col], numbers[1][col], numbers[0][col])
+            moves = moves + move
+
+    if moves > 0:
+        ajouter_tuile()
+        display_game()
+        if is_win():
+            won=True
+            messagebox.showinfo("", "You win ")
+
+        if is_game_full() and count_mergeable() == 0:
+            messagebox.showinfo("", "You lose")
+
+# code qui permet de quitter le jeu
+    if key == "Q" or key == "q":
+        result = messagebox.askokcancel("Confirmation", "Voulez-vous vraiment quitter ?")
+        if result:
+            quit()
 
 def pack4(a, b, c, d):
     moves = 0
@@ -44,28 +135,7 @@ def pack4(a, b, c, d):
         d=0
         moves = moves + 1
 
-
-
     return a, b, c, d, moves
-
-print(pack4(0, 4, 8, 0))
-'''
-numbers = [
- [2, 4, 8, 16],
- [32, 64, 128, 256],
- [512, 1024, 2048, 0],
- [0, 0, 0, 0],
-]
-'''
-
-
-
-numbers = [
- [0, 0, 0, 0],
- [0, 0, 0, 0],
- [0, 0, 0, 0],
- [0, 0, 0, 0],
-]
 
 def is_win():
     if won is True:
@@ -155,7 +225,6 @@ colours = {
     1024: "#FEEF81",
     2048: "#FEEF4D",
 }
-won = False
 
 x0 = 200 # horizontal beginning of labels
 y0 = 100 # vertical beginning of labels
@@ -169,6 +238,8 @@ win.title(" exemple labels positionnés par .place")
 btn_reset = Button(win,text="Reset",command=reset)
 btn_reset.place(x=500, y=40,width=300, height=50)
 Label(text="2048", width=10, height=1, font=("Arial", 60)).place(x=50, y=8)
+Label(text="Q = Quitter", width=22, height=1, font=("Arial", 30)).place(x=10, y=730)
+Label(text="Z = Retour en arrière", width=30, height=1, font=("Arial", 30)).place(x=10, y=790)
 # labels creation and position (1. Creation 2. position)
 for line in range(len(numbers)):
     for col in range(len(numbers[line])):
@@ -177,57 +248,10 @@ for line in range(len(numbers)):
         # label positionning in the windows
         labels[line][col].place(x=x0 + width * col, y=y0 + height * line)
 
-def display_game():#Chat gpt je lui est demandé trouve moi le code
-    for line in range(len(numbers)):
-        for col in range(len(numbers[line])):
-            # Met à jour le texte et la couleur de fond du label en fonction de la valeur dans `numbers`
-            labels[line][col].config(
-                text=numbers[line][col] or "",
-                bg=colours.get(numbers[line][col], "#74E2AE")
-            )
-            # Affectation des touches aux fonctions, q pour quitter, le reste pour "tasser" dans une certaine direction
 
-def key_pressed(event):
-    global won
-    key = event.keysym  # récupérer le symbole de la touche
 
-    moves = 0
-    #code qui permet de faire les mouvement gauche, droite, haut, bas avec les touches w,a,s,d
-    if key == "Right" or key == "d" or key == "D":
-        for line in range(len(numbers)):
-            numbers[line][3], numbers[line][2], numbers[line][1], numbers[line][0], move = pack4(numbers[line][3],numbers[line][2],numbers[line][1],numbers[line][0])
-            moves = moves + move
-    if key == "Left" or key == "a" or key == "A":
-        for line in range(len(numbers)):
-            numbers[line][0], numbers[line][1], numbers[line][2], numbers[line][3], move = pack4(numbers[line][0],numbers[line][1],numbers[line][2],numbers[line][3])
-            moves = moves + move
-    if key == "Up" or key == "w" or key == "W":
-        for col in range(len(numbers[0])):
-            numbers[0][col], numbers[1][col], numbers[2][col], numbers[3][col], move = pack4(numbers[0][col], numbers[1][col], numbers[2][col], numbers[3][col])
-            moves = moves + move
-    if key == "Down" or key == "s" or key == "S":
-        for col in range(len(numbers[0])):
-            numbers[3][col], numbers[2][col], numbers[1][col], numbers[0][col], move = pack4(numbers[3][col], numbers[2][col], numbers[1][col], numbers[0][col])
-            moves = moves + move
 
-    if moves > 0:
-        ajouter_tuile()
-        display_game()
-        if is_win():
-            won=True
-            messagebox.showinfo("", "You win ")
-
-        if is_game_full() and count_mergeable() == 0:
-            messagebox.showinfo("", "You lose")
-
-# code qui permet de quitter le jeu
-    if key == "Q" or key == "q":
-        result = messagebox.askokcancel("Confirmation", "Voulez-vous vraiment quitter ?")
-        if result:
-            quit()
 
 # permet d'appeler key_pressed avec n'importe quelle touche
 win.bind("<Key>", key_pressed)
 win.mainloop()
-
-
